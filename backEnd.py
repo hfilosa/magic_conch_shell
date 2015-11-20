2f741280db8ba7f4ccf8670b502ebce59e81c135
Winton Yee <wyee@github.com>
2015-11-20 10:10:21 -0500

    backEnd

--- backEnd.py
+++ backEnd.py
@@ -6,10 +6,36 @@ l=[]
 for result in r:
     l.append(result)
 
-print l[0]
+#print l[0]
 
-url = urllib2.urlopen(l[0])
-page = url.read()
-soup = bs4.BeautifulSoup(page,'html')
-raw = soup.get_text()
-print raw
+def get_pages(l):
+    n = 0
+    while n < len(l):
+        url = urllib2.urlopen(l[n])
+        page = url.read()
+        soup = bs4.BeautifulSoup(page,'html')
+        raw = soup.get_text()
+        n = n+1
+        #print raw
+    return raw
+
+def find_name(s):
+    exp = "[A-Z][a-z]+ [A-Z][a-z]+"
+    result = re.findall(exp,s)
+    return result
+
+#print find_name(raw)
+
+def find_champ(l):
+    for item in l:
+        champ = ""
+        champnum = 0
+        current = l.count(item)
+        if  current > champnum:
+            champnum = current
+            champ = item
+    return champ
+
+pages = get_pages(l)
+names = find_name(pages)
+print find_champ(names)
